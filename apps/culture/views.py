7afff23b921c811522e66a51993414e08b2102a9
imanol <imanol@zenitsolar.eu>
2024-07-01 19:29:36 +0200

    wip

--- apps/culture/views.py
+++ apps/culture/views.py
@@ -19,9 +19,9 @@ def play_console(request, **kwargs):
                 ],
                 "target": {
                 "namespace": "android_app",
-                "package_name": "app.run.a.opendataeuskadi_z6zzxsqmma_ew.twa",
+                "package_name": "eus.maps.kultur.twa",
                 "sha256_cert_fingerprints": [
-                    "94:BE:68:73:23:3D:22:94:5E:07:9B:3B:7A:E5:ED:B2:43:62:86:89:02:BB:BC:A7:FB:97:EE:DA:FE:AD:67:6A"
+                    "2C:B1:EA:52:EA:48:DC:7B:A7:F2:C3:1E:30:8F:DB:9F:33:7C:B1:24:A2:07:C4:C3:15:33:2E:A4:0A:12:3D:B9"
                 ]
                 }
             }
